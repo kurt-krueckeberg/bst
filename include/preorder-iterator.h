5479013956432b144cb5ed03c84cacaf1019f846
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2020-03-10 15:36:47 -0400

    Add iterator__preorder::increment()
    Finished iterator_preorder. 
    Added increment() method and sentinel comparison methods

--- include/preorder-iterator.h
+++ include/preorder-iterator.h
@@ -1,108 +1,115 @@
-/* 
- * Converted Pre-order Java iterator. This is NOT an in-order iterator!!
- *
- * The Binary Trees lecture is at:
- * 
- * https://www.cs.cmu.edu/~adamchik/15-121/lectures/Trees/trees.html
- *
- * The BST code with the Pre-order iterator class was taken from:
- */
-class preorder_stack_iterator {
-    
+
+
+// TODO: Make this class a friend of bstree.
+class iterator_preorder {  // This not efficient to copy due to the stack container inside it.
+
    using node_type = bstree<Key, Value>::node_type;
-   
-   std::stack<node_type*> stack; 
-   
+
    node_type *current;
 
-   const bstree<Key, Value>& tree;
+   bstree<Key, Value>& tree;
 
-   iterator& increment() noexcept 
+   //enum class position : char { start, in_between, end };  
+   position pos;
+
+   Node *increment() 
    {
-      Node *current = stack.top();
-      
-      if (current->left)       // If left not nullptr, push it onto stack.  
-      
-          stack.push(current->left.get());
-      
-      else {
-      
-          Node *tmp = stack.top();
+     Node *__y = current;
 
-          stack.pop(); 
-      
-          while (!tmp->right) {  // While tmp->right is nullptr
-      
-             if (stack.empty()) 
-                  return *this;  // current is properly set
-      
-             tmp = stack.top();
-             stack.pop();  
-          }
-      
-          stack.push(tmp->right.get());
-      }
-      
-      return *this;  
-   }  
+     if (__y->left) // Prefer left child
+         __y = __y->left.get();
 
- public:
-   // traits for forward iterator
+     else if (__y->right) // otherwise, the right 
+         __y = __y->right.get();
+
+     else  { // current is a leaf node, find parent that is left childIf leaf node, ascend until ? 
+         auto parent = __y->parent;
+         while (__y == parent->right.get()) {
+		 
+		 // TODO: special case when the parent is the root.
+		 __y = parent;
+                 parent = __y->parent;
+	 }
+	 __y = parent; // __y is now ancestor parent and current is in its left subtree.
+	 if (__y->right)
+             __y = __y->right.get();
+	 else    
+             __y = current; // There is no next pre-order node.		   
+     }     
+     return __y;
+   }
+   
+  public:
+
    using difference_type  = std::ptrdiff_t; 
-   using value_type       = tree234<Key, Value>::value_type; 
+   using value_type       = bstree<Key, Value>::value_type; 
    using reference        = value_type&; 
    using pointer          = value_type*;
-   
+       
    using iterator_category = std::forward_iterator_tag; 
-	
-   preorder_stack_iterator(const bstree<Key, Value>& lhs) : tree{lhs}
-   {
-      if (tree.root) {
 
-          stack.push(root);
-          increment();  // Go to first node to retrieve
-      }
-   }
- 
-   preorder_stack_iterator& operator++() noexcept 
+   explicit iterator_preorder(bstree<Key, Value>& bstree) : tree{bstree}
    {
-      increment();
+      // Set current to nodee with smallest key.
+      current = bstree.root.get();
+
+   }
+   
+   iterator_preorder(const iterator_preorder& lhs) : current{lhs.current}, tree{lhs.tree}, pos{lhs.pos}
+   {
+   }
+   
+   iterator_preorder(iterator_preorder&& lhs) : current{lhs.current}, tree{lhs.tree}
+   {
+       lhs.current = nullptr;
+   }
+   
+   iterator_preorder& operator++() noexcept 
+   {
+      current = increment();
       return *this;
    } 
    
-   preorder_stack_iterator operator++(int) noexcept
+   iterator_preorder operator++(int) noexcept
    {
-      preorder_stack_iterator tmp(*this);
-   
-      increment();
-   
+      iterator_preorder tmp(*this);
+
+      current = increment();
+
       return tmp;
    } 
-     
+          
    reference operator*() const noexcept 
    { 
-       return current->__get_value();
+       return current->__get_value(); // May want 'Node *' itself
    } 
    
    pointer operator->() const noexcept
    { 
       return &(operator*()); 
    } 
+   
+   struct sentinel {}; // Use for determining "at the end" in 'bool operator==(const iterator_preorder&) const' below
 
-   struct sentinel {};
-
-   bool operator==(const sentinel& sent) const noexcept
+   bool operator==(const iterator_preorder::sentinel& sent) const noexcept
    {
-      return !stack.empty();
+      return (current == increment()) ? true: false; 
    }
-
-   bool operator!=(const sentinel& sent) const noexcept
+   
+   bool operator!=(const iterator_preorder::sentinel& lhs) const noexcept
    {
-      return !operator==(sent);
+     return !operator==(lhs);    
    }
+};
+iterator_preorder begin() noexcept
+{
+   iterator_preorder iter{*this}; 
+   return iter; 
+}
 
-   value_type& operator*() 
-   {
-       return current->__get_value();
-   }
-};   
+iterator_preorder::sentinel end() noexcept // TODO: Can I use a sentinel? a C++17 feature.
+{
+    typename iterator_preorder::sentinel sent;
+    return sent;
+}
+
