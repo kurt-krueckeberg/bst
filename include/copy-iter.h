d256cda3597d17149ac0796362928c199e59d0b0
Kurt Krueckeberg <kurt.krueckeberg@gmail.com>
2020-03-18 10:50:50 -0400

    Start copy iterators

---
+++ include/copy-iter.h
@@ -0,0 +1,130 @@
+#ifndef __copy_iterators_h_
+#define __copy_iterators_h_
+/*
+    New code to replace copy_tree() with:
+
+      siter = tree.begin_copy(???);
+    
+      siter_end = tree.end_copy();
+    
+      diter = siter.make_dest_iterator();
+    
+      copy(siter, siter_end, diter);
+      OR
+      move(siter, siter_end, diter);
+    }
+
+*/
+
+template<typename Key, typename Value>
+class copy_source_iterator output_iterator_tag {
+
+  private:
+      bstree<Key, Value>&  dest_tree;
+      copy_source_iterator<Key, Value>& __src; // Can we eliminate this?
+      
+  public:
+     
+  using iterator_category = std::output_iterator_tag; 
+
+  copy_source_iterator(copy_src_iterator& src_iter) : __src{src_iter}, dest_tree{__src.dest_tree}
+  {
+  }
+
+  // Copy constructor.
+  copy_source_iterator(const copy_source_iterator& __obj) : __src{__obj.__src}, dest_tree{__obj.dest_tree} 
+  { 
+  }
+
+  copy_source_iterator& operator=(const Node * __y)
+  {
+     std::unique_ptr<Node> ptr = std::make_unique<Node>(__y->__vt); // copy source node
+ 
+    if (!__y->parent) // If _y is the root of the source tree, we know ptr set to nullptr above by ctor.
+       
+        dest_tree.root = std::move(ptr); 
+ 
+    else if (__src.dest_parent->key() > ptr->key()) // dest_node is left child  
+           
+        __src.dest_parent->connectLeft(ptr); 
+           
+    else // new node is a right child
+           
+        __src.dest_parent->connectRight(ptr); 
+ 
+    __src.dest_parent->key() 
+
+    return *this;
+  }
+
+  copy_source_iterator&
+  operator*()
+  {
+  }
+
+  copy_source_iterator&
+  operator++()
+  {
+  }
+
+  copy_source_iterator&
+  operator++(int)
+  {
+  }
+};
+
+template<typename Key, typename Value>
+class copy_dest_iterator output_iterator_tag {
+public:
+
+    private:
+      bstree<Key, Value>&  dest_tree;
+      copy_source_iterator<Key, Value>& __src; // Can we eliminate this?
+      
+    public:
+     
+  using iterator_category = std::output_iterator_tag; 
+
+  copy_dest_iterator(copy_src_iterator& src_iter) : __src{src_iter}, dest_tree{__src.dest_tree}
+  {
+  }
+
+  // Copy constructor.
+  copy_dest_iterator(const copy_dest_iterator& __obj) : __src{__obj.__src}, dest_tree{__obj.dest_tree} 
+  { 
+  }
+
+  copy_dest_iterator& operator=(const Node * __y)
+  {
+     std::unique_ptr<Node> ptr = std::make_unique<Node>(__y->__vt); // copy source node
+ 
+    if (!__y->parent) // If _y is the root of the source tree, we know ptr set to nullptr above by ctor.
+       
+        dest_tree.root = std::move(ptr); 
+ 
+    else if (__src.dest_parent->key() > ptr->key()) // dest_node is left child  
+           
+        __src.dest_parent->connectLeft(ptr); 
+           
+    else // new node is a right child
+           
+        __src.dest_parent->connectRight(ptr); 
+ 
+    __src.dest_parent->key() 
+
+    return *this;
+  }
+
+  copy_dest_iterator&
+  operator*()
+  { return *this; }
+
+  copy_dest_iterator&
+  operator++()
+  { return *this; }
+
+  copy_dest_iterator&
+  operator++(int)
+  { return *this; }
+};
+#endif
